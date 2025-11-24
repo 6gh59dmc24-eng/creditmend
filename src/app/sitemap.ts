a39039e268828b9610be763fa0ef1b48a231565b
SoftwarePros <6gh59dmc24@privaterelay.appleid.com>
2025-11-23 19:25:07 -0600

    Update domain to onboard.creditmend.org

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://creditmend.org';
+  const baseUrl = 'https://onboard.creditmend.org';
 
   return [
     {
